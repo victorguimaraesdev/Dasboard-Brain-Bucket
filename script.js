fd6fbcebecb3ff916660b68b095be5baffaa58fe
victorguimaraesdev <victorguimaraesmax@gmail.com>
2025-01-04 16:44:43 -0300

    Adicionado funcionalidade ao botão excluir

--- script.js
+++ script.js
@@ -3,3 +3,9 @@ document.getElementById('criarBotao').addEventListener('click', () => {
     let divTarefaClone = divTarefaPadrao.cloneNode(true);
     document.getElementById('containerPrincipal').appendChild(divTarefaClone);
 })
+
+document.getElementById('excluirBotao').addEventListener('click', () => {
+    let containerPrincipal = document.getElementById('containerPrincipal');
+    let ultimaTarefaAdicionada = containerPrincipal.lastElementChild;
+    ultimaTarefaAdicionada.remove();
+})
