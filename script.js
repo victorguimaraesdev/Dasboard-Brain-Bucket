8256138c88235d93e707f8b3476a3891f160a305
victorguimaraesdev <victorguimaraesmax@gmail.com>
2025-01-05 09:36:43 -0300

    Adicionado funcionalidade de seleção de tarefas

--- script.js
+++ script.js
@@ -1,11 +1,32 @@
-document.getElementById('criarBotao').addEventListener('click', () => {
-    let divTarefaPadrao = document.getElementById('divTarefaPadrao');
-    let divTarefaClone = divTarefaPadrao.cloneNode(true);
-    document.getElementById('containerPrincipal').appendChild(divTarefaClone);
-})
+//seleção
+let selecionar = null;
+function addSelecao(div) {
+  div.addEventListener("click", () => {
+    if (selecionar) {
+      selecionar.classList.remove("selecionada");
+    }
+    selecionar = div;
+    selecionar.classList.add("selecionada");
+  });
+}
 
-document.getElementById('excluirBotao').addEventListener('click', () => {
-    let containerPrincipal = document.getElementById('containerPrincipal');
-    let ultimaTarefaAdicionada = containerPrincipal.lastElementChild;
-    ultimaTarefaAdicionada.remove();
-})
+//Criar nova tarefa
+document.getElementById("criarBotao").addEventListener("click", () => {
+  let divTarefaPadrao = document.getElementById("divTarefaPadrao");
+  let divTarefaClone = divTarefaPadrao.cloneNode(true);
+  divTarefaClone.removeAttribute('id')
+  addSelecao(divTarefaClone);
+  document.getElementById("containerPrincipal").appendChild(divTarefaClone);
+});
+//Excluir tarefa
+document.getElementById("excluirBotao").addEventListener("click", () => {
+  if (selecionar) {
+    selecionar.remove();
+    selecionar = null;
+  }
+    else{
+        alert("Selecione uma tarefa");
+    }
+});
+//Add função
+adicionarSelecao(document.getElementById('divTarefaPadrao'));
